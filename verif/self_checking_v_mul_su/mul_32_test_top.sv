89f5bd1e4b2daf204c1e7ab3580019e74a5c855b
abdullah Jhatial <abdullahjhatial92@gmail.com>
2024-12-24 02:37:49 +0500

    synthesis and Sim flow tested

--- verif/self_checking_v_mul_su/mul_32_test_top.sv
+++ verif/self_checking_v_mul_su/mul_32_test_top.sv
@@ -1,5 +1,4 @@
-
-module test_32_bit_mul();
+module mul_32_test_top();
     // Control signals
     logic [1:0] opcode;          // Operation code for the multiplication
     logic [1:0] precision;       // Precision level for multiplication
@@ -8,7 +7,8 @@ module test_32_bit_mul();
     logic [31:0] mul_out_32;     // Output of the multiplication
     logic rst;                   // Reset signal
     logic clk = 1;               // Clock signal initialized to 1
-
+    int  actual_a;
+    int actual_b;
     // Intermediate signals
     logic [63:0] al;             // Result of multiplication (64 bits)
     int i;                       // Loop index
@@ -16,9 +16,19 @@ module test_32_bit_mul();
     logic [31:0] bit_16;         // Temporary variable for 16-bit multiplication
     int fail = 0;                // Counter for failed tests
     int pass = 0;                // Counter for passed tests
-
+    logic [63:0] x;
+    int mulpass;
+    int mulfail;
+    int mulhpass;
+    int mulhfail;
+    int mulhsupass; 
+    int mulhsufail;
+    int mulhupass;
+    int mulhufail;
+   ///////// corner case list//////////////
+  
     // Instantiate the 32-bit multiplication unit
-    mul_32bit_precision_control mul_dut (
+    v_mult_su mul_dut (
         .clk(clk),
         .rst(rst),
         .operand_a_reg(operand_a_t),
@@ -38,38 +48,55 @@ module test_32_bit_mul();
     logic [3:0] sign_b;
 
     // Include multiplication tasks
-    `include "mulh.sv"
-    `include "mul.sv"
-    `include "mulhu.sv"
-    `include "mulsu.sv"
+    `include "/home/abdullah/vmult_signed_unsigned_by_Vedic_MULTIPLIER/verif/self_checking_v_mul_su/mulh_test.sv"
+    `include "/home/abdullah/vmult_signed_unsigned_by_Vedic_MULTIPLIER/verif/self_checking_v_mul_su/mulhu_test.sv"
+    `include "/home/abdullah/vmult_signed_unsigned_by_Vedic_MULTIPLIER/verif/self_checking_v_mul_su/mulsu_test.sv"
+    `include "/home/abdullah/vmult_signed_unsigned_by_Vedic_MULTIPLIER/verif/self_checking_v_mul_su/mul_test.sv"
 
     initial begin
         // Initialize waveform dump
         $dumpfile("file.vcd");
         $dumpvars();
-
+  
         // Reset the system
-        rst = 1'b0;
-        #1;
         rst = 1'b1;
-
-        // Test cases
-        precision = 2'b01; // Set precision for multiplication
-        opcode = 2'b10;    // Set opcode for unsigned multiplication
-      //  mulhu();           // Call the unsigned multiplication task
-$display("pass=%d   fail=%d",pass,fail);
-  
-        opcode = 2'b01;    // Set opcode for signed multiplication
-    //    mulh();            // Call the signed multiplication task
-$display("pass=%d   fail=%d",pass,fail);
-  
-        opcode = 2'b00;    // Set opcode for regular multiplication
-        mul();             // Call the regular multiplication task
-$display("pass=%d   fail=%d",pass,fail);
- 
-        opcode = 2'b11;    // Set opcode for signed multiplication with unsigned
-        mulhsu();          // Call the signed multiplication with unsigned task
-      $display("pass=%d   fail=%d",pass,fail);
+        #1;
+        rst = 1'b0;
+      #1;
+      rst = 1'b1; 
+      
+     precision=2'b01;
+     operand_a_t=32'h0f05561f;  
+     operand_b_t=32'h04b58300;
+     
+     opcode=2'b00;
+     mul();
+    
+     mulpass=pass;
+     mulfail=fail; 
+     pass=0;
+     fail=0;
+     opcode=2'b01;
+     mulh();
+     mulhpass=pass;
+    mulhfail=fail; 
+     pass=0;
+     fail=0;
+     opcode=2'b10;
+     mulhu();
+     mulhupass=pass;
+     mulhufail=fail;
+     pass=0;
+     fail=0;
+     opcode = 2'b11;    
+     mulhsu();             
+     mulhsupass=pass;
+     mulhsufail=fail;
+     
+      $display(" mulpass=%d   mulhpass=%d    mulhupass=%d     mulhsupass=%d \n mulfail=%d   mulhfail=%d  mulsufail=%d    mulhsufail=%d  ",mulpass,mulhpass,mulhupass, mulhsupass,mulfail,mulhfail,  mulhsufail, mulhsufail);
   $finish;
     end
 endmodule
+
+
+
